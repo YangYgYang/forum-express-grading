5a8d605d1c9713a7f3af691c9b1076c2db11c10a
YangYgYang <you@example.com>
2022-12-01 21:45:17 +0800

    update followship model error name,and model association name

--- models/user.js
+++ models/user.js
@@ -21,13 +21,13 @@ module.exports = (sequelize, DataTypes) => {
         foreignKey: 'userId',
         as: 'LikedRestaurants'
       })
-      User.belongsToMany(models.User, {
-        through: models.Followership,
+      User.belongsToMany(User, {
+        through: models.Followship,
         foreignKey: 'followerId',
         as: 'Followings'
       })
-      User.belongsToMany(models.User, {
-        through: models.Followership,
+      User.belongsToMany(User, {
+        through: models.Followship,
         foreignKey: 'followingId',
         as: 'Followers'
       })
